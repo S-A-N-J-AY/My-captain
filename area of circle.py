49428d5ba0282a0707a1c2a2997e6096beddd914
S-A-N-J-AY <99339867+S-A-N-J-AY@users.noreply.github.com>
2022-02-11 23:38:37 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 11 18:45:38 2022
+
+@author: mandeep
+"""
+
+pi=3.14
+
+R=float(input('enter radius: '))
+
+area=(pi*R*R)
+
+print("the area of circle is", area)
---
+++ file extention.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 11 23:11:24 2022
+
+@author: mandeep
+"""
+
+Filename=input("enter the filename: " )
+
+p= Filename.split(".")
+
+print("Extention of the file is : "+p[-1])
